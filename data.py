c2530df2abf8f614bcbd24a38bf0dff0c722f1b6
hsq <hsqyc@163.com>
2018-09-18 22:32:12 +0800

    add visualization

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def resize_img(img):
@@ -31,26 +32,42 @@ def resize_img(img):
     return im_sized
 
 
-img = cv2.imread('QUANZHOU_Level_19.tif_res_0.71_27_rotate270.tif')
-# img = resize_img(img)
+def rotate_box(xmin, xmax, ymin, ymax, angle):
+    rot_mat = cv2.getRotationMatrix2D(((xmin + xmax) / 2, (ymin + ymax) / 2), angle, 1)
+    a = np.dot(rot_mat, np.array([[xmin], [ymin], [1]]))
+    b = np.dot(rot_mat, np.array([[xmax], [ymin], [1]]))
+    c = np.dot(rot_mat, np.array([[xmax], [ymax], [1]]))
+    d = np.dot(rot_mat, np.array([[xmin], [ymax], [1]]))
 
-x = 148.820997
-y = 162.968333
-w = 63.035310
-h = 13.029849
+    return a[0][0], a[1][0], b[0][0], b[1][0], c[0][0], c[1][0], d[0][0], d[1][0]
 
-xmin = int(x - w / 2)
-xmax = int(x + w / 2)
-ymin = int(y - h / 2)
-ymax = int(y + h / 2)
 
-origin_img_sized = img  # [:, :, ::-1]
-origin_img_sized = origin_img_sized.copy()
-cv2.rectangle(origin_img_sized, (xmin, ymin), (xmax, ymax), (0, 0, 255), 1)
-# for y in range(51):
-#     cv2.line(origin_img_sized, (0, 8 * (y + 1)), (416, 8 * (y + 1)), (0, 0, 255), 1)
-# for x in range(51):
-#     cv2.line(origin_img_sized, (8 * (x + 1), 0), (8 * (x + 1), 416), (0, 0, 255), 1)
-cv2.imwrite('out.bmp', origin_img_sized)
+def visualization(img_path):
+    img = cv2.imread(img_path)
+    origin_img_sized = img  # [:, :, ::-1]
+    origin_img_sized = origin_img_sized.copy()
 
-cv2.getRotationMatrix2D()
+    with open(img_path + '.rbox', 'r') as fh:
+        for line in fh:
+            line = line.strip().split(' ')
+
+            x = float(line[0])
+            y = float(line[1])
+            w = float(line[2])
+            h = float(line[3])
+            angle = float(line[5])
+
+            xmin = int(x - w / 2)
+            xmax = int(x + w / 2)
+            ymin = int(y - h / 2)
+            ymax = int(y + h / 2)
+
+            x1, y1, x2, y2, x3, y3, x4, y4 = rotate_box(xmin, xmax, ymin, ymax, angle)
+            cv2.line(origin_img_sized, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 255), 1)
+            cv2.line(origin_img_sized, (int(x2), int(y2)), (int(x3), int(y3)), (0, 0, 255), 1)
+            cv2.line(origin_img_sized, (int(x3), int(y3)), (int(x4), int(y4)), (0, 0, 255), 1)
+            cv2.line(origin_img_sized, (int(x4), int(y4)), (int(x1), int(y1)), (0, 0, 255), 1)
+    cv2.imwrite('out1.bmp', origin_img_sized)
+
+
+visualization('QUANZHOU_Level_19.tif_res_0.71_16_rotate270.tif')
