3f7213741e2caa6b51b847c287da596583588451
黄世青 <hsqyc@163.com>
2018-09-26 17:04:16 +0800

    [batch_size,h,w,3]

--- train.py
+++ train.py
@@ -11,6 +11,7 @@ import os
 
 def yolo_loss(y_pred, y_true):
     batch_size = Gb_batch_size
+    anchors = tf.constant(Gb_anchors, dtype='float', shape=[1, 1, 1, 9, 2])
 
     loss = 0
     sum_loss_xy = 0
@@ -18,17 +19,19 @@ def yolo_loss(y_pred, y_true):
     sum_loss_c = 0
     sum_loss_angle = 0
 
-    cellbase_y = tf.to_float(tf.reshape(tf.tile(tf.range(52), [52]), (1, 52, 52, 1, 1, 1)))
-    cellbase_x = tf.transpose(cellbase_y, (0, 2, 1, 3, 4, 5))
-    cellbase_grid = tf.tile(tf.concat([cellbase_x, cellbase_y], -1), [batch_size, 1, 1, 3, 6, 1])
+    cellbase_x = tf.to_float(tf.reshape(tf.tile(tf.range(52), [52]), (1, 52, 52, 1, 1, 1)))
+    cellbase_y = tf.transpose(cellbase_x, (0, 2, 1, 3, 4, 5))
+    cellbase_grid = tf.tile(tf.concat([cellbase_x, cellbase_y], -1), [batch_size, 1, 1, 6, 3, 1])
     for i in range(3):
+        anchor = anchors[..., 3 * i:3 * (i + 1), :]
         object_mask = y_true[i][..., 4:5]
-        grid_w = tf.shape(y_pred[i])[1]  # 13
-        grid_h = tf.shape(y_pred[i])[2]  # 13
+        grid_h = tf.shape(y_pred[i])[1]  # 13
+        grid_w = tf.shape(y_pred[i])[2]  # 13
         grid_factor = tf.reshape(tf.cast([grid_w, grid_h], tf.float32), [1, 1, 1, 1, 1, 2])
-        net_out_reshape = tf.reshape(y_pred[i], [-1, grid_w, grid_h, 3, 6, 6])
+        net_out_reshape = tf.reshape(y_pred[i], [-1, grid_h, grid_w, 6, 3, 6])
 
-        adjusted_true_xy = y_true[i][..., :2] * grid_factor - cellbase_grid[:, :grid_w, :grid_h, ...]
+        adjusted_true_xy = y_true[i][..., :2] * grid_factor - cellbase_grid[:, :grid_h, :grid_w, ...]
+        adjusted_out_wh = tf.exp(net_out_reshape[..., 2:4]) * anchor / img_factor
 
         loss_xy = tf.reduce_sum(
             object_mask * xywh_scale * tf.nn.sigmoid_cross_entropy_with_logits(logits=net_out_reshape[..., :2],
